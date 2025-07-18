1986b3cc36ff6a559a00930eaa9c39fa0d29288d
Daksa Hardistya <ngurahdaksa72@gmail.com>
2025-07-19 01:40:22 +0800

    perbaikan kedua text

--- resources/views/metodePembayaran.blade.php
+++ resources/views/metodePembayaran.blade.php
@@ -238,19 +238,18 @@
                         // Notifikasi WhatsApp
                         const adminPhone = '085777115304';
                         const fonnteToken = 'cFh96YKghJi8GQkN3LFN';
-
                         const pesanAdmin = `🛒 *Order Baru Masuk!*
-👤 *Data Pelanggan:*
+*Data Pelanggan:*
 • Nama    : ${customerData.nama}
 • Telepon : ${customerData.nomor_tlp}
 • Alamat  : ${customerData.alamat}
-📦 *Rincian Belanjaan:*
+*Rincian Belanjaan:*\n
 *Produk:*
-${orderProduk.map((item, i) => `• ${i + 1}. ${item.nama_produk} x${item.jumlah_produk} - Rp ${item.harga_produk.toLocaleString('id-ID')}`).join('\n')}
- ${orderPaket.length > 0 ? `\n🎁 *Paket:*\n${orderPaket.map((item, i) => `• ${i + 1}. ${item.nama_paket} x${item.jumlah_paket} - Rp ${item.harga_paket.toLocaleString('id-ID')}`).join('\n')}` : ''}
-💳 *Pembayaran:* ${tipePembayaran}
-🧾 *Nomor Transaksi:* ${nomorPembayaran}
-💰 *Total:* Rp ${totalHarga.toLocaleString('id-ID')}`;
+${orderProduk.map((item, i) => `• ${i + 1}. ${item.nama_produk} x${item.jumlah_produk} - Rp ${item.harga_produk.toLocaleString('id-ID')}`).join('\n')}${orderPaket.length > 0 ? `\n
+    *Paket:*\n${orderPaket.map((item, i) => `• ${i + 1}. ${item.nama_paket} x${item.jumlah_paket} - Rp ${item.harga_paket.toLocaleString('id-ID')}`).join('\n')}` : ''}
+*Pembayaran:*${tipePembayaran}
+*Nomor Transaksi:*${nomorPembayaran}
+*Total:*Rp ${totalHarga.toLocaleString('id-ID')}`;
 
                         fetch("https://api.fonnte.com/send", {
                             method: "POST",
